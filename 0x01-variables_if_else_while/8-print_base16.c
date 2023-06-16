71741cfe96e12d79b8d07ecb2567212c57415d47
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-06-16 09:58:30 -0700

    Added a program that prints all the numbers of base 16 in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry Point
  *
- * Description: Prints all the numbers of base 16 in lowercase, followed by a new line
+ * Description: Prints all the numbers of base 16 in lowercase, 
+ * followed by a new line.
  *
  * Return: Always 0 (Success)
  */
