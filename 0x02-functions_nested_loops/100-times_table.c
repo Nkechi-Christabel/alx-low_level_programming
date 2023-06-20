0b4aec133ad6083aa3cf9a2e73f7c1d3dffb9857
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-06-20 14:46:10 -0700

    Added a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,7 +24,6 @@ void print_times_table(int n)
 				if (j != 0)
 				{
 				_putchar(' ');
-				_putchar(' ');
 				}
 				_putchar('0' + times);
 			}
@@ -38,6 +37,7 @@ void print_times_table(int n)
 			if (j != 9)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 			}
 		}
