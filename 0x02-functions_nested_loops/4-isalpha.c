35f1a2f44716c9494b166d2c98567b1fd8573950
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-06-20 05:45:38 -0700

    Added a function that checks for uppercase character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 #include <ctype.h>
 
 /**
- * _islower - The program checks if the charater is uppercase or not.
+ * _isalpha - The program checks if the charater is uppercase or not.
  *
  *@c: The character to be checked.
  *
  * Return: Returns 1 or 0, depending on the char.
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	return (isalpha(c) ? 1 : 0);
 }
