dc65b4c49e8392e87a0635e6d615fc9d31e541fc
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-06-21 13:39:16 -0700

    I fixed the print_remaining_days() function so that the output works correctly for all dates and all leap years.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,43 +1,26 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Prints even  Fibonacci numbers below 4,000,000.
+ * main - A program that finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned long int i, j, k, j1, j2, k1, k2;
+	int i;
+	unsigned long num1 = 1, num2 = 2;
 
-	j = 1;
-	k = 2;
-
-	printf("%lu", j);
-
-	for (i = 1; i < 91; i++)
+	for (i = 0; i < 96; i += 2)
 	{
-		printf(", %lu", k);
-		k = k + j;
-		j = k - j;
+		printf("%lu, %lu, ", num1, num2);
+
+		num1 = num1 + num2;
+		num2 = num1 + num2;
 	}
 
-	j1 = j / 1000000000;
-	j2 = j % 1000000000;
-	k1 = k / 1000000000;
-	k2 = k % 1000000000;
-
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
-		k1 = k1 + j1;
-		j1 = k1 - j1;
-		k2 = k2 + j2;
-		j2 = k2 - j2;
-	}
-
-	printf("\n");
+	printf("%lu", num1);
+	printf(", %lu\n", num2);
 
 	return (0);
 }
