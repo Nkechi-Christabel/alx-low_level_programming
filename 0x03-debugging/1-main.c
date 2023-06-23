7e3e2ad02c5cf6bf4c1a46df3e999224f0550f3e
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-06-23 12:49:31 -0700

    I unkowingly replaced the content in 1-main.c file thinking it's a testing file. It's now updated accordingly.

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
-* main - takes a date and prints how many days are left in the year, taking
-* leap years into account
+* main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
