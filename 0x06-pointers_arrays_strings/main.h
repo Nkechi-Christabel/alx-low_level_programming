8b70e203ce9efbb86770ae3ac729270a03bf7072
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-06-29 09:44:09 -0700

    Added  a function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,1 @@
-void reverse_array(int *a, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _putchar(char c);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
+char *rot13(char *);
