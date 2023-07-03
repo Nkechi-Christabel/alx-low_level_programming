10456a59db2868c513b2226ad09b58612941bdab
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-03 11:25:52 -0700

    Wrote a function that sets the value of a pointer to a char.

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,7 @@
 /**
  * set_string - The program sets the value of a pointer to a char.
- *s: The pointer to set to a char.
- *to: The cahr to se the pointer to.
+ *@s: The pointer to set to a char.
+ *@to: The cahr to se the pointer to.
  *
  */
 void set_string(char **s, char *to)
