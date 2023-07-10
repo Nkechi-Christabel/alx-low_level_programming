1af3e1764766348fd38822850677c45de570c1bb
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-10 09:37:07 -0700

    Removed unsed header file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include "main.h"
 
 /**
  * create_array - The program creates an array of chars, and initializes it
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,2 +1,3 @@
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+= malloc((strlen(str) + 1) * si    zeof(char));
