f8b51e580cfd4f6353372fe7decd029668d9c843
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-10 17:36:57 -0700

    Wrote a function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,15 +13,14 @@
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int i, j, len;
+	int i, len;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0, len = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
+		len += strlen(av[i]);
 	}
 
 	len += ac + 1;
