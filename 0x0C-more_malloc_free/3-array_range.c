36ec168b83f254b423fc3b539bb9520747f085c3
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-13 06:44:11 -0700

    Wrote a function that creates an array of integers.

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -5,18 +5,19 @@
  * @min: Minimum value.
  * @max: Maximum value.
  *
- * Return: Pointer to the allocated memory on success or NULL if it fails.
+ * Return: Pointer to newly created array on success or NULL if it fails.
  */
 int *array_range(int min, int max)
 {
-	int *arr, i;
+	int *arr, i, size;
 
 	if (min > max)
 		return (NULL);
 
-	arr = malloc((max + 1) * sizeof(int));
+	size = max - min + 1;
+	arr = malloc(size * sizeof(int));
 
-	for (i = 0; arr != NULL && i <= max; i++)
+	for (i = 0; arr != NULL && i <= size; i++)
 	{
 		arr[i] = i;
 	}
