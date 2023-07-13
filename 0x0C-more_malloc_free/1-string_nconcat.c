5e60e6d53d74e58754ceddf0375448dc69757caf
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-13 04:57:51 -0700

    Wrote a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,16 +13,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
 	unsigned int len1 = strlen(s1), len2 = strlen(s2);
-	unsigned int len = n >= len2 ? (len1 + len2) : (len1 + n);
 
-	str = malloc(len + 1 * sizeof(char));
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	n >= len2 ? (n = len2) : n;
+
+	str = malloc((len1 + n + 1) * sizeof(char));
 
 	if (str != NULL)
 	{
 		strcpy(str, s1);
-
-		if (n >= len2)
-			strcat(str, s2);
 
 		strncat(str, s2, n);
 	}
