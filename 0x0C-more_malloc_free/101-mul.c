a9727c4212ba4238da4243ace2749ac571558783
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-13 16:15:35 -0700

    Wrote a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,16 +7,16 @@
  * mul - The function multiplies two numbers as strings
  * @num1: First number as a string
  * @num2: Second number as a string
- * @result: To store the result of the multiplication
+ * @mem: Allocated memory to store the result of the multiplication
  *
  * Return: Multiple of two numbers
  */
 
-void mul(char *num1, char *num2, char *result)
+void mul(char *num1, char *num2, char *mem)
 {
 	int len1 = strlen(num1), len2 = strlen(num2), start;
 	int totalLen = len1 + len2;
-	int *temp = (int *)calloc(totalLen, sizeof(int));
+	int *temp_mem = (int *)calloc(totalLen, sizeof(int));
 	int i, j, digit1, digit2, product;
 
 	for (i = len1 - 1; i >= 0; i--)
@@ -27,45 +27,45 @@ void mul(char *num1, char *num2, char *result)
 			digit2 = num2[j] - '0';
 			product = digit1 * digit2;
 
-			temp[i + j + 1] += product % 10;
-			temp[i + j] += product / 10;
+			temp_mem[i + j + 1] += product % 10;
+			temp_mem[i + j] += product / 10;
 		}
 	}
 
 	for (i = totalLen - 1; i > 0; i--)
 	{
-		if (temp[i] >= 10)
+		if (temp_mem[i] >= 10)
 		{
-			temp[i - 1] += temp[i] / 10;
-			temp[i] %= 10;
+			temp_mem[i - 1] += temp_mem[i] / 10;
+			temp_mem[i] %= 10;
 		}
 	}
 
 	start = 0;
-	while (temp[start] == 0 && start < totalLen - 1)
+	while (temp_mem[start] == 0 && start < totalLen - 1)
 		start++;
 
 	for (i = start, j = 0; i < totalLen; i++, j++)
-		result[j] = temp[i] + '0';
+		mem[j] = temp_mem[i] + '0';
 
-	result[totalLen - start] = '\0';
+	mem[totalLen - start] = '\0';
 
-	free(temp);
+	free(temp_mem);
 }
 
 /**
  * validate_args - validate the arguments passed into the program
- * @av: Array of strings that is the arguments
+ * @argv: Array of strings that are the arguments
  */
-void validate_args(char **av)
+void validate_args(char *argv[])
 {
 	int i, j;
 
 	for (i = 1; i < 3; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(av[i][j]))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				exit(98);
@@ -84,7 +84,7 @@ void validate_args(char **av)
 int main(int argc, char *argv[])
 {
 	char *num1 = argv[1], *num2 = argv[2], *mem;
-	int totalLen;
+	int len;
 
 	if (argc != 3)
 	{
@@ -93,8 +93,8 @@ int main(int argc, char *argv[])
 	}
 	validate_args(argv);
 
-	totalLen = strlen(num1) + strlen(num2);
-	mem = malloc(sizeof(char) * (totalLen + 1));
+	len = strlen(num1) + strlen(num2);
+	mem = malloc(sizeof(char) * (len + 1));
 
 	mul(num1, num2, mem);
 	printf("%s\n", mem);
