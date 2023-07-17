ff058d78965ea96676f5dfe236c0f4cf6caf9956
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-17 06:02:25 -0700

    Wrote a function that frees dogs.

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,37 +1,13 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * new_dog - The program creates a new dog
- * @name: An element in the struct dog
- * @age: An element in the struct dog
- * @owner: An element in the struct dog
- *
- * Return: POinter to a new dog
+ * free_dog - The program frees dogs.
+ * @dog_t: Pointer to the dog to be freed
  */
-dog_t *new_dog(char *name, float age, char *owner)
+void free_dog(dog_t *d)
 {
-	dog_t *newDog = malloc(sizeof(dog_t));
-
-	if (newDog)
-	{
-		newDog->name = malloc(strlen(name) + 1);
-		newDog->owner = malloc(strlen(owner) + 1);
-		newDog->age = age;
-
-		strcpy(newDog->name, name);
-		strcpy(newDog->owner, owner);
-	}
-
-	if (newDog->name == NULL || newDog->owner == NULL)
-	{
-		free(newDog->name);
-		free(newDog->owner);
-		free(newDog);
-
-		return (NULL);
-	}
-
-	return (newDog);
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
