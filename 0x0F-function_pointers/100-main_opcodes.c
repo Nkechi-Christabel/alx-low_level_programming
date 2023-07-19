2a9b9ced0f4171afdd56fc92672efe24259792cd
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-19 07:50:56 -0700

    Wrote a program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,16 +5,16 @@
 /**
  * print_opcodes - The program prints the opcodes of its own main function.
  * @func_ptr: Pointer to the function whose opcodes are to be printed.
- * @num: The number of bytes to print.
+ * @num_bytes: The number of bytes to print.
  *
  * Return: The opcodes of the provided function
  */
-void print_opcodes(void (*func_ptr)(int), int num)
+void print_opcodes(void (*func_ptr)(int), int num_bytes)
 {
 	char *ptr = (unsigned char *)func_ptr;
 	int i;
 
-	for (i = 0; i < num; i++)
+	for (i = 0; i < num_bytes; i++)
 		printf("%02hhx ", ptr[i] & 0xFF);
 
 	printf("\n");
@@ -29,7 +29,7 @@ void print_opcodes(void (*func_ptr)(int), int num)
  */
 int main(int argc, char *argv[])
 {
-	int num;
+	int num_bytes;
 
 	if (argc != 2)
 	{
@@ -37,15 +37,17 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	num = atoi(argv[1]);
+	num_bytes = atoi(argv[1]);
 
-	if (num < 0)
+	if (num_bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	print_opcodes((void (*)(int))main, num);
+	void (*print_opcodes_ptr)(int) = (void (*)(int))main;
+
+	print_opcodes(print_opcodes_ptr, num_bytes);
 
 	return (0);
 }
