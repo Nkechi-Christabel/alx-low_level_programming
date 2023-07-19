e9971d97f46d94abd569c418e6d2490989e95ae0
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-19 05:09:05 -0700

    Wrote a function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,11 @@
-#include "function_pointers.h"
-
 /**
  * print_name - The program prints a name
  * @name: The name to printed
  * @f: The function pointer with a char parameter
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name || f)
 		(*f)(name);
 }
