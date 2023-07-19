21fb9d38ebf355fe1983f1380de920a62b59e612
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-19 06:58:24 -0700

    Wrote a program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * print_opcodes - The program prints the opcodes of its own main function.
+ * @num: The number of bytes passed as an argument.
+ *
+ * Return: The opcodes of its own main function.
+ */
+
+void print_opcodes(int num)
+{
+	char *ptr = (char *)print_opcodes;
+	int i;
+
+	for (i = 0; i < num; i++)
+		printf("%02x ", ptr[i] & 0xFF);
+
+	putchar('\n');
+}
 
 /**
  * main - The program that prints the opcodes of its own main function.
@@ -7,11 +27,22 @@
  *
  * Return - 0 always (Success)
  */
+
 int main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
-		prntf("Error\n");
+		printf("Error\n");
 		exit(1);
-	
+	}
 
+	if (argv[1] < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	print_opcodes(atoi(argv[1]));
+
+	return (0);
+}
