d1b872b8abae898e3d0915faddfac729f6e0f6fc
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-27 11:10:19 -0700

    Wrote a function that executes before the main function does.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,9 +4,9 @@
  * prints_before_main - The program prints before the main function
  * is executed.
  */
-void prints_before_main() __attribute__((constructor));
+void prints_before_main(void) __attribute__((constructor));
 
-void prints_before_main()
+void prints_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
