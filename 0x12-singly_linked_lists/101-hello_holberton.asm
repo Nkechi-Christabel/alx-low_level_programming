098e220c37b9fc3b14d29d85327f11494c9e2d8b
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-27 12:14:53 -0700

    Wrote a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,26 @@
+section .data
+    hello db "Hello, Holberton,", 0
+    format db "%s\n", 0
+
+section .text
+    global main
+
+extern printf
+extern exit
+
+main:
+    ; Prepare the stack frame
+    push rbp
+    mov rbp, rsp
+
+    ; Call printf function
+    mov rdi, format
+    mov rsi, hello
+    call printf
+
+    ; Clean up and exit
+    xor edi, edi  ; Return code 0
+    call exit
+
+section .bss
+    resb 8  ; Reserve 8 bytes for exit's return value
