bdd7b52adde17680e94012170d1abcb49b31a888
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-08-01 05:05:18 -0700

    Wrote a function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,40 +9,27 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t index;
     size_t node_count = 0;
-    listint_t *next_node, *current = *h;
-    listint_t **visited_nodes = malloc(sizeof(listint_t *));
-
-    if (!visited_nodes)
-        exit(98);
+    listint_t *current = *h;
+    listint_t *next_node;
 
     while (current)
     {
         next_node = current->next;
-        
-        for (index = 0; index < node_count; index++)
-        {
-            if (visited_nodes[index] == current)
-            {
-                free(visited_nodes);
-                *h = NULL;
-                return node_count;
-            }
-        }
 
-        visited_nodes = realloc(visited_nodes, (node_count + 1) * sizeof(listint_t *));
-        if (!visited_nodes)
-            exit(98);
-        visited_nodes[node_count] = current;
+        current->next = NULL;
+        free(current);
+
+        current = next_node;
         node_count++;
 
-        free(current);
-        current = next_node;
+        if (current == *h)
+        {
+            *h = NULL;
+            break;
+        }
     }
 
-    free(visited_nodes);
-    *h = NULL;
-    return node_count;
+    return (node_count);
 }
 
