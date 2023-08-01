d78402e13a0531816a68dc57022968c9e3b3a225
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-08-01 05:35:02 -0700

    Wrote a function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * free_listint_safe - Frees a listint_t linked list.
@@ -9,41 +10,38 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t node_count = 0, index;
-	listint_t *next, *current = *h;
-	listint_t **visited_nodes = malloc(sizeof(listint_t *));
+    size_t node_count = 0;
+    listint_t *current = *h;
+    listint_t *next_node;
+    listint_t **visited_nodes = malloc(sizeof(listint_t *) * 1024);
 
-	if (!visited_nodes)
-		exit(98);
+    if (!visited_nodes)
+        exit(98);
 
-	while (current)
-	{
-		next = current->next;
+    while (current)
+    {
+        next_node = current->next;
 
-		for (index = 0; index < node_count; index++)
-		{
-			if (visited_nodes[index] == current)
-			{
-				free(visited_nodes);
-				*h = NULL;
-				return (node_count);
-			}
-		}
-		visited_nodes = realloc(visited_nodes, (node_count + 1) *
-				sizeof(listint_t *));
+        size_t index;
+        for (index = 0; index < node_count; index++)
+        {
+            if (visited_nodes[index] == current)
+            {
+                free(visited_nodes);
+                *h = NULL;
+                return node_count;
+            }
+        }
 
-		if (!visited_nodes)
-			exit(98);
+        visited_nodes[node_count] = current;
+        node_count++;
 
-		visited_nodes[node_count] = current;
-		node_count++;
+        free(current);
+        current = next_node;
+    }
 
-		free(current);
-		current = next;
-	}
-
-	free(visited_nodes);
-	*h = NULL;
-
-	return (node_count);
+    free(visited_nodes);
+    *h = NULL; 
+    return node_count;
 }
+
