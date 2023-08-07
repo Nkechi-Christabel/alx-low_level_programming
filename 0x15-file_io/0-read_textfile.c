8f9074c3f0aed03ce155e1d57a9831939879052a
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-08-07 06:02:51 -0700

    Wrote  a function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @letters: The number of letters it should read and print.
+ * @filename: The file to read.
+ *
+ * Return: Actual number of letters it could read and print or 0 if it fails
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t read_fl, write_fl;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	buf = malloc(letters);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	read_fl = read(fd, buf, letters);
+
+	if (read_fl == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	buf[read_fl] = '\0';
+
+	write_fl = write(STDOUT_FILENO, buf, read_fl);
+
+	free(buf);
+
+	if (write_fl == read_fl)
+		return (0);
+
+	close(fd);
+
+	return (read_fl);
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,4 @@
+<h1>File I/O</h1>
+<p>System calls are the calls that a program makes to the system kernel to provide the services to which the program does not have direct access. For example, providing access to input and output devices such as monitors and keyboards. We can use various functions provided in the C Programming language for input/output system calls such as create, open, read, write, etc.</p>
+<h2>What is the File Descriptor?</h2>
+<p>In Unix type systems, a file descriptor (fd for short) is a small positive integer used as reference to an open file in a process. The file descriptor is an integer that uniquely identifies an open file of the process.</p>
