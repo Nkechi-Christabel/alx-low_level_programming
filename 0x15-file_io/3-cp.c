2656404052f588ce9b152fa2ffcf0ad3cf047311
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-08-07 15:51:46 -0700

    Updated a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-		error_exit("Usage: %s file_from file_to\n", "", 97);
+		error_exit("Usage: file_from file_to\n", "", 97);
 
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
@@ -56,16 +56,10 @@ int main(int argc, char *argv[])
 	if (i == -1)
 		error_exit("Error: Can't read from file %s\n", argv[1], 98);
 
-	if (close(file_from) == -1)
+	if (close(file_from) == -1 || close(file_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (file_from == -1)
+				? file_to : file_from);
 		exit(100);
 	}
-
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
-	return (0);
 }
