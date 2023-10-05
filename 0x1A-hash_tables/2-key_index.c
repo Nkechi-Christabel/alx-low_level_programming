b6b3b81ba6f988c3611838947e78f95ec03346b7
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-05 13:07:46 -0700

    Fixed betty errors for 1-djb2 file

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_table.c"
+
+/**
+ * key_index - It Gives the index of a key.
+ * @key: The key.
+ * @size: The size of the array of the hash table.
+ *
+ * Return: The index at which the key/value pair should
+   be stored in the array of the hash table.
+ */
+unsigned long int key_index(const unsigned char *key,
+		unsigned long int size)
+{
+
