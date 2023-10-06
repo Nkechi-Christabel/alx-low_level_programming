88fff0e0414c97c55402a42179b143ef3a5b6ee6
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-06 03:21:07 -0700

    Updated a function that prints a hash table.

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -15,7 +15,7 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i])
+		while (ht->array[i])
 		{
 			if (printed)
 				printf(", ");
