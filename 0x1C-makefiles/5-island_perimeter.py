eb5d5e9ff6ea4a5c499be7ed425f15815a23b8c6
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-11-22 10:18:47 -0800

    Created a function def island_perimeter(grid): that returns the perimeter of the island described in grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,8 +13,10 @@ def island_perimeter(grid):
 
     Constraints:
     - Grid is rectangular, with width and height not exceeding 100.
-    - Grid is completely surrounded by water, and there is one island (or nothing).
-    - The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
+    - Grid is completely surrounded by water, and there is one island
+    (or nothing).
+    - The island doesn’t have “lakes” (water inside that isn’t connected to
+    the water around the island).
     """
 
     perimeter = 0
