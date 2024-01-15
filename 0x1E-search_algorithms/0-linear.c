d660218e28e78ea88b024056f4ecc144851765ce
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-15 15:25:56 -0800

    Added functions on search algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,11 +13,14 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	for (i = 0; array && i < size; i++)
+	if (array)
 	{
-		printf("array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			printf("array[%ld] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (i);
+		}
 	}
 
 	return (-1);
